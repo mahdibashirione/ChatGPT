acad995c382cabf3c3545674ab8bde247e36b0f3
mahdibashirione <mahdi94250@gmail.com>
2023-04-10 22:18:36 +0430

    handle effect typing

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,12 +2,16 @@ import Typewriter from 'typewriter-effect';
 
 const Message = ({ message }) => {
 
+  const hr = new Date().getHours()
+  const min = new Date().getMinutes()
+  const time = hr + ":" + min
+
   return (
     <div className={`w-full flex flex-col ${message.direction === "outgoing" ? "items-end" : "items-start"}`}>
       <p className={` ${message.direction === "outgoing" ? "bg-blue-500 before:bg-blue-500 before:-right-0.5 text-white" : "bg-zinc-800 text-gray-400 before:-left-0.5 before:bg-zinc-800"} 
-    before:content-[""] before:bottom-2 before:w-4 before:h-4 before:absolute before:rotate-45 before:-z-10
-    max-w-[85%] px-3 py-2 text leading-7 rounded-xl relative z-10 overflow-hidden`}>
-        {message.sender === "user" ? message.message :
+    before:content-[""] before:bottom-1.5 before:w-4 before:h-4 before:absolute before:rotate-45 before:-z-10
+    max-w-[85%] px-3 py-2 text leading-7 rounded-xl relative z-10`}>
+        {message.sender === "ChatGPT" && message.sendTime == time ?
           <Typewriter
             options={{
               strings: message.message,
@@ -15,9 +19,10 @@ const Message = ({ message }) => {
               cursor: "",
               loop: false,
             }}
-          />}
+          /> : message.message
+        }
       </p>
-      <span className="text-gray-600 text-[12px]">{message.sendTime}</span>
+      <span className="text-gray-600 text-[12px] select-none">{message.sendTime}</span>
     </div>
   );
 }
